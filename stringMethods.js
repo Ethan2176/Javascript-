7d31c190b09c9d9661d863d2972041758aa8c6a0
Ethan2176 <ethan.nathaniel201@gmail.com>
2025-03-03 22:44:22 +0530

    yes

---
+++ stringMethods.js
@@ -0,0 +1,46 @@
+// string methods
+
+let UserName = "Ethan";
+
+console.log(UserName.charAt(2)); // will return character at that index
+
+console.log(UserName.indexOf("n")); // Returns position of first occurrence of a character
+
+console.log(UserName.lastIndexOf("n")); // Returns position of last occurrence of a character
+
+console.log(UserName.length); // Returns length of a string.
+
+UserName1 ="    Ethan   ";
+
+console.log(UserName1.trim()); // To remove any whitespaces
+
+console.log(UserName.toUpperCase()); // To convert entire string to uppercase.
+
+console.log(UserName.toLowerCase()); // To convert entire string to lowercase.
+
+console.log(UserName.repeat(5)); // To repeat a string a certain number of times.
+
+console.log(UserName.startsWith("E")); // To check if a string starts with a particular character
+
+console.log(UserName.endsWith(" ")); // To check if a string ends with a particular character;
+
+let phonenumber ="69-6999-69996-666";
+
+console.log(phonenumber.replaceAll("-","")); // To replace all instances of the first character with the second character.
+
+console.log(phonenumber.replaceAll("-","/"));
+
+console.log(phonenumber.padStart(24,"1")); // To add a particular character to the start till the string becomes a certain length.
+
+console.log(phonenumber.padEnd(26,"4"));
+
+
+
+
+
+
+
+
+
+
+
