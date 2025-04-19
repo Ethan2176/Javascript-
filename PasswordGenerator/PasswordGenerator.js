9a22864872fbc557d4cd221961c468bd8b813d85
Ethan2176 <ethan.nathaniel201@gmail.com>
2025-04-19 12:29:18 +0530

    yes

--- PasswordGenerator/PasswordGenerator.js
+++ PasswordGenerator/PasswordGenerator.js
@@ -3,7 +3,7 @@ let Password="";
 const Button = document.getElementById("MySubmit");
 const Output = document.getElementById("MyLabel");
 const Reset = document.getElementById("MyReset");
-
+const Copy = document.getElementById("MyCopy")
 Button.onclick = function RandomPassword(value)
 { 
     value = window.prompt("Enter the length of your password");
@@ -19,5 +19,19 @@ Reset.onclick = function ResetWebsite()
     Password="";
     Output.textContent="";
 }
+
+Copy.onclick = function()
+{
+    navigator.clipboard.writeText(Password)
+    .then(function() {
+        // Alert the user that the text has been copied
+        alert('Text copied to clipboard: ' + Password);
+    })
+    .catch(function(err) {
+        console.error('Failed to copy: ', err);
+    });
+};
+
+
 
 
